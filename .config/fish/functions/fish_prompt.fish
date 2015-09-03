2dffacee5876a4e2cca8460ceb380059ea94344f
dave <davidsamuellaw@gmail.com>
2015-09-02 20:14:18 -0700

    Batmang

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,62 +1,63 @@
-# name: clearance
-# ---------------
-# Based on idan. Display the following bits on the left:
-# - Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
-# - Current directory name
-# - Git branch and dirty state (if inside a git repo)
+# Based on clearance and batman
 
 function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
 function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
 end
 
-
 function fish_prompt
-  set -l last_status $status
+    set -l last_status $status
 
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l normal (set_color normal)
+    set -l cyan (set_color cyan)
+    set -l yellow (set_color yellow)
+    set -l red (set_color red)
+    set -l blue (set_color blue)
+    set -l green (set_color green)
+    set -l normal (set_color normal)
 
-  set -l cwd $green(pwd)
+    set -l cwd $green(pwd)
 
-  # Output the prompt, left to right
+    # Output the prompt, left to right
 
-  # Add a newline before new prompts
-  echo -e ''
+    # Add a newline before new prompts
+    echo -e ''
 
-  # Show git branch and status
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
+    # Show git branch and status
+    if [ (_git_branch_name) ]
+        set -l git_branch (_git_branch_name)
 
-    if [ (_git_is_dirty) ]
-      set git_info $blue '[ ' $yellow $git_branch "±" $blue ' ]'
-    else
-      set git_info $blue '[ ' $green $git_branch $blue ' ]'
+        if [ (_git_is_dirty) ]
+            set git_info $blue '[ ' $yellow $git_branch "±" $blue ' ]'
+        else
+            set git_info $blue '[ ' $green $git_branch $blue ' ]'
+        end
+        echo -n -s $git_info $normal '    '
     end
-    echo -n -s $git_info $normal '    '
-  end
 
-  set -l prompt_color $red
-  if test $last_status = 0
-    set prompt_color $normal
-  end
+    #set -l prompt_color $red
+    #if test $last_status = 0
+    #    set prompt_color $normal
+    #end
 
-  # Display [venvname] if in a virtualenv
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b cyan black) '[ ' (basename "$VIRTUAL_ENV") ' ]' $normal ' '
-  end
+    # Display [ venvname ] if in a virtualenv
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b cyan black) '[ ' (basename "$VIRTUAL_ENV") ' ]' $normal ' '
+    end
 
-  # Print pwd or full path
-  echo -n -s $cwd $normal
+    # Print pwd or full path
+    echo -n -s $cwd $normal
 
-  # Terminate with a nice prompt char
-  echo -e ''
-  echo -e -n -s $prompt_color '> ' $normal
+    # Terminate with batman prompt
+    echo -e ''
+    for color in (begin
+        test $last_status -ne 0
+            and printf "%s\n" 600 900 c00
+            or printf "%s\n"  333 666 aaa
+        end)
+        printf "%s"(set_color $color)">"
+    end
+    echo -e -n -s ' '
 end
