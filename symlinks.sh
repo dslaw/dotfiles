b0cce395bfc66275d3b4cd9abda1d831bb74560f
David Law <davidsamuellaw@gmail.com>
2017-06-16 20:51:54 -0700

    Update setup script
    Remove old dotfile `.bpython`
    Use current path instead of hardcoding

--- symlinks.sh
+++ symlinks.sh
@@ -3,16 +3,14 @@
 # Create symlinks from dotfiles in ~/dotfiles to home directory
 # Backs up preexisting dotfiles into ~/dotfiles_old first
 
-dir=~/dotfiles
 backup=~/dotfiles_old
 files=".aliases .bashrc .config/fish .xinitrc .Xresources \
        .i3 .compton.conf .config/ranger/rc.conf \
-       .bpython .ipython_config.py .Rprofile .vimrc"
+       .ipython_config.py .Rprofile .vimrc"
 
 mkdir -p $backup
 
 for file in $files; do
     mv ~/$file ~/dotfiles_old/ 2>/dev/null
-    ln -s $dir/$file ~/$file 2>/dev/null
+    ln -s $PWD/$file ~/$file 2>/dev/null
 done
-
