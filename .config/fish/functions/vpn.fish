ce4ba17a36715df2034022433610f97f84026b63
David Law <davidsamuellaw@gmail.com>
2019-05-06 21:21:28 -0700

    Remove vpn function

--- .config/fish/functions/vpn.fish
+++
@@ -1,6 +0,0 @@
-function vpn
-  sudo openvpn --config "/etc/openvpn/$argv[1].ovpn" \
-    --auth-user-pass "/etc/openvpn/pia.txt" \
-    --ca "/etc/openvpn/ca.rsa.2048.crt" \
-    --crl-verify "/etc/openvpn/crl.rsa.2048.pem"
-end
